93c626e8e13144f67bcd8ee735a63570ee18c8b2
jothiaishu <jothiaishu2311@gmail.com>
2026-02-17 06:48:05 -0500

    initial commit

---
+++ basic.py
@@ -0,0 +1,16 @@
+#basic syntax in python
+print("Im expert in python")
+
+#variables (dynamic typing)
+subject="english"
+age=27
+is_result=True
+weight=65.5
+print(weight)
+
+#datatypes(int,float,string,boolean)
+marks=100
+pts=35.7
+colour="red"
+flag=True
+print(type(colour))
---
+++ feb_17.py
@@ -0,0 +1,26 @@
+#reverse a string
+def reverse_string(s):
+    return s[::-1]
+print(reverse_string("hello")) 
+
+#check if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+print(is_prime(11))
+
+#fibonacci sequence
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+print(fibonacci(10))
+
+
